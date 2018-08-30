edfe7de49d75b8964d5427d838910536654af71d
Engin Bozaba <enginbozaba@gmail.com>
2018-08-30 17:05:43 +0300

    Update grnn.py

--- grnn.py
+++ grnn.py
@@ -44,3 +44,29 @@ print('Sonuç :' ,model.predict())
 print('Std : ',model.std)
 print('Ağırlık :',model.weight())
 print('MSE : ',model.mean_squared_error())
+
+print("-----------------------------------------")
+
+train_x = np.array([[1,2,3],[3,4,5],[5,6,7],[7,8,9]])
+train_y = np.array([4,6,8,10]).T
+test_x= np.array([[2,3,4]])
+test_y =np.array([5])
+
+model=GRNN(train_x,train_y,test_x,test_y)
+print('Sonuç :' ,model.predict())
+print('Std : ',model.std)
+print('Ağırlık :',model.weight())
+print('MSE : ',model.mean_squared_error())
+
+"""
+C:\Users\Engin Bozaba>python "c:/Users/Engin Bozaba/Desktop/vs.py"
+Sonuç : 7.0
+Std :  [[1. 1. 1. 1.]]
+Ağırlık : [[5.00796571e-159 1.11089965e-002 1.11089965e-002 5.00796571e-159]]
+MSE :  [0.]
+-----------------------------------------
+Sonuç : 4.999999999999999
+Std :  [[1. 1. 1. 1.]]
+Ağırlık : [[1.11089965e-002 1.11089965e-002 5.00796571e-159 0.00000000e+000]]
+MSE :  [7.88860905e-31]
+"""
